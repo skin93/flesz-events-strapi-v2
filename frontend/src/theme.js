c144c6452ce1b335081db667a39550fa78c81edd
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-07-05 21:43:50 +0200

    Restore theme

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,108 +1,117 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import white from '@material-ui/core/colors';
+import grey from '@material-ui/core/colors/grey';
+import teal from '@material-ui/core/colors/teal';
 
 const theme = createMuiTheme({
-  // overrides: {
-  //   MuiCssBaseline: {
-  //     '@global': {
-  //       figure: {
-  //         textAlign: 'center',
-  //         margin: '30px 0',
-  //         '& img': {
-  //           width: '60%',
-  //           height: 'auto',
-  //         },
-  //       },
-  //       '.iframe-container': {
-  //         position: 'relative',
-  //         overflow: 'hidden',
-  //         width: '100%',
-  //         paddingTop: '56.25%',
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        figure: {
+          display: 'flex',
+          justifyContent: 'center',
+          textAlign: 'left',
+          fontSize: 'calc(.7rem + .5vw)',
+          lineHeight: '1.5',
+          margin: '30px',
+          '& img': {
+            width: '60%',
+            height: 'auto',
+          },
+        },
+        '.raw-html-embed': {
+          position: 'relative',
+          overflow: 'hidden',
+          width: '100%',
+          paddingTop: '56.25%',
 
-  //         '& iframe': {
-  //           position: 'absolute',
-  //           top: '50%',
-  //           left: '50%',
-  //           transform: 'translate(-50%,-50%)',
-  //           width: '80%',
-  //           height: '80%',
-  //         },
-  //       },
-  //       body: {
-  //         padding: 0,
-  //         margin: 0,
-  //         backgroundColor: '#fff',
-  //       },
-  //       blockquote: {
-  //         fontWeight: 600,
-  //         fontStyle: 'italic',
-  //         quotes: `"“" "”"`,
-  //         position: 'relative',
-  //         '&::before': {
-  //           content: 'open-quote',
-  //           position: 'absolute',
-  //           top: 60,
-  //           left: -40,
-  //           fontSize: '6em',
-  //           color: '#32e0c4',
-  //         },
-  //         backgroundColor: 'transparent',
-  //         color: `rgba(255, 255, 255, 0.9)`,
-  //         padding: `30px`,
-  //         lineHeight: `2em`,
-  //         margin: `30px auto`,
-  //         width: `80%`,
-  //         fontSize: 'calc(.7rem + .5vw)',
-  //       },
-  //       h2: {
-  //         color: `#eee`,
-  //         fontWeight: 'bold',
-  //         fontSize: 'calc(1rem + 1vw)',
-  //       },
-  //       p: {
-  //         fontSize: 'calc(.7rem + .5vw)',
-  //         lineHeight: '1.5',
-  //       },
-  //       a: {
-  //         color: '#32e0c4',
-  //         textDecoration: `none`,
-  //       },
-  //       strong: {
-  //         color: `#eee`,
-  //       },
-  //       ul: {
-  //         paddingLeft: '15px',
-  //         margin: '30px 0',
-  //       },
-  //       ol: {
-  //         paddingLeft: '15px',
-  //         margin: '30px 0',
-  //       },
-  //       li: {
-  //         margin: `10px 0`,
-  //         fontSize: 'calc(.7rem + .5vw)',
-  //         '&::marker': {
-  //           color: '#32e0c4',
-  //         },
-  //       },
-  //       ul: {
-  //         li: {
-  //           listStyle: `square`,
-  //         },
-  //       },
-  //     },
-  //   },
-  // },
+          '& iframe': {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%,-50%)',
+            width: '80%',
+            height: '80%',
+          },
+        },
+        body: {
+          padding: 0,
+          margin: 0,
+          backgroundColor: grey[900],
+        },
+        blockquote: {
+          fontWeight: 600,
+          fontStyle: 'italic',
+          quotes: '"“" "”"',
+          position: 'relative',
+          '&::before': {
+            content: 'open-quote',
+            position: 'absolute',
+            top: 60,
+            left: -40,
+            fontSize: '6em',
+            color: teal.A400,
+          },
+          backgroundColor: 'transparent',
+          color: 'rgba(255, 255, 255, 0.9)',
+          padding: '30px',
+          lineHeight: '2em',
+          margin: '0 auto',
+          width: '80%',
+          fontSize: 'calc(.7rem + .5vw)',
+        },
+        h2: {
+          color: '#eee',
+          fontWeight: 'bold',
+          fontSize: 'calc(1rem + 1vw)',
+        },
+        p: {
+          fontSize: 'calc(.7rem + .5vw)',
+          lineHeight: '1.5',
+          color: grey[400],
+        },
+        span: {
+          backgroundColor: 'transparent',
+        },
+        a: {
+          color: teal.A400,
+          textDecoration: 'none',
+        },
+        strong: {
+          color: '#eee',
+        },
+        ul: {
+          paddingLeft: '0px',
+          margin: '0 auto',
+        },
+        ol: {
+          paddingLeft: '0xp',
+          margin: '0 auto',
+        },
+        li: {
+          lineHeight: '1.5',
+          fontSize: 'calc(.7rem + .5vw)',
+          '&::marker': {
+            color: teal.A400,
+          },
+        },
+        ul: {
+          li: {
+            listStyle: 'square',
+          },
+        },
+      },
+    },
+  },
   palette: {
     primary: {
-      main: '#32e0c4',
+      main: teal.A400,
     },
-    type: 'light',
+    type: 'dark',
     accent: {
-      main: '#32e0c4',
+      main: teal.A400,
     },
     light: {
-      main: '#eee',
+      main: grey[200],
     },
     black: {
       main: '#212121',
@@ -116,16 +125,15 @@ const theme = createMuiTheme({
       lighter: 'rgb(40,40,40)',
     },
   },
-  // typography: {
-  //   fontFamily: ['Lato', 'sans-serif'].join(','),
-  //   h1: {
-  //     fontSize: `3rem`,
-  //   },
-  //   subtitle1: {
-  //     fontSize: `1.2rem`,
-  //     fontWeight: `bold`,
-  //   },
-  // },
+  typography: {
+    h1: {
+      fontSize: '3rem',
+    },
+    subtitle1: {
+      fontSize: '1.2rem',
+      fontWeight: 'bold',
+    },
+  },
 });
 
 export default theme;
