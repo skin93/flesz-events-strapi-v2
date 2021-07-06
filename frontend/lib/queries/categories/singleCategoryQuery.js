5a15dcf4b0ffab51ae18f62b2a492e8b8c63221a
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-07-06 19:01:18 +0200

    Display articles based on category name

---
+++ frontend/lib/queries/categories/singleCategoryQuery.js
@@ -0,0 +1,35 @@
+import { gql } from 'graphql-request';
+export const SINGLE_CATEGORY_QUERY = gql`
+  query singleCategoryQuery($slug: String!) {
+    categories(where: { slug: $slug }) {
+      name
+      description
+      slug
+      articles {
+        id
+        title
+        slug
+        image_cover {
+          alternativeText
+          url
+        }
+        category {
+          name
+          slug
+        }
+        published_at
+      }
+      metadata {
+        meta_title
+        meta_description
+        share_image {
+          media {
+            url
+            caption
+            alternativeText
+          }
+        }
+      }
+    }
+  }
+`;
