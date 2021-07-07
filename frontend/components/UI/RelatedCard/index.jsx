0de92d7867230da063716a62cb184a6313113ca0
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-07-07 14:13:00 +0200

    Modify RelatedCard

--- frontend/components/UI/RelatedCard/index.jsx
+++ frontend/components/UI/RelatedCard/index.jsx
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import Typography from '@material-ui/core/Typography';
 
 import { getMediaUrl } from '@/lib/getMediaUrl';
@@ -12,26 +11,22 @@ const RelatedCard = ({ article }) => {
   const classes = useStyles();
   return (
     <Card className={classes.root}>
-      <CardActionArea>
-        <div className={classes.box}>
-          <CardMedia
-            component='img'
-            className={classes.media}
-            image={getMediaUrl(article.image_cover)}
-            title={article.title}
-            alt={article.title}
-          />
-          <CardContent className={classes.content}>
-            <Typography
-              gutterBottom
-              variant='subtitle2'
-              component='h4'
-              className={classes.title}>
-              {article.title}
-            </Typography>
-          </CardContent>
-        </div>
-      </CardActionArea>
+      <CardMedia
+        component='img'
+        className={classes.media}
+        image={getMediaUrl(article.image_cover)}
+        title={article.title}
+        alt={article.title}
+      />
+      <CardContent className={classes.content}>
+        <Typography
+          gutterBottom
+          variant='subtitle2'
+          component='h4'
+          className={classes.title}>
+          {article.title}
+        </Typography>
+      </CardContent>
     </Card>
   );
 };
@@ -41,25 +36,27 @@ export default RelatedCard;
 const useStyles = makeStyles((theme) => ({
   root: {
     position: 'relative',
-    width: '300px',
+    width: '100%',
     height: '100px',
-    margin: '10px auto',
-  },
-  box: {
-    display: 'flex',
-    flexDirection: 'row',
   },
   media: {
-    height: '100px',
-    width: '300px',
+    height: '100%',
   },
   content: {
-    width: '100%',
+    position: 'absolute',
+    top: 0,
+    left: 0,
     display: 'flex',
-    justifyContent: 'flex-start',
+    flexDirection: 'column',
+    justifyContent: 'center',
     alignItems: 'center',
-    height: 100,
-    backgroundColor: theme.palette.background.lighter,
+    backgroundColor: 'rgba(0,0,0,0.7)',
+    transition: 'all .2s ease-in-out',
+    '&:hover': {
+      backgroundColor: 'rgba(0,0,0,0.5)',
+    },
+    width: '100%',
+    height: '100%',
   },
   title: {
     fontWeight: 'bold',
@@ -67,7 +64,7 @@ const useStyles = makeStyles((theme) => ({
     margin: 0,
     fontSize: 'calc(9px + 0.2vw)',
     [theme.breakpoints.up('lg')]: {
-      fontSize: '12px',
+      fontSize: '13px',
     },
   },
 }));
