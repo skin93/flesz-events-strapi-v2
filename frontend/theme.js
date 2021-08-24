c893c1468b8edbb20b710c20b78a5efaf09b7054
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-08-24 19:16:56 +0200

    Fix padding in MuiCardContent

--- frontend/theme.js
+++ frontend/theme.js
@@ -3,6 +3,15 @@ import grey from '@material-ui/core/colors/grey';
 
 const theme = createTheme({
   overrides: {
+    MuiCardContent: {
+      root: {
+        padding: '1rem 1.5rem',
+
+        '&:last-child': {
+          paddingBottom: '1rem',
+        },
+      },
+    },
     MuiDialog: {
       paperScrollBody: {
         textAlign: 'center',
