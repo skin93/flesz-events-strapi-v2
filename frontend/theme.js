2cdfe148fb2fad6563dc226210c5b4e801ba554a
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-08-31 12:21:14 +0200

    Make MuiDialog widder

--- frontend/theme.js
+++ frontend/theme.js
@@ -21,6 +21,9 @@ const theme = createTheme({
       paperScrollBody: {
         textAlign: 'left',
       },
+      paperWidthSm: {
+        maxWidth: '800px',
+      },
     },
     MuiDialogTitle: {
       root: {
