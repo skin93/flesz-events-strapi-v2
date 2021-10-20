63c6a12936d886cdd556de0c5706892f4bddc80c
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-10-20 16:42:46 +0200

    Disable eslint druign build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,4 +4,9 @@ module.exports = {
   images: {
     domains: [process.env.NEXT_PUBLIC_DOMAIN],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
