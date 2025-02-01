2446612d3aab9feb026eac987cd020bb90b8ed7c
skin93 <arro.ciesielski@gmail.com>
2025-02-01 20:35:22 +0100

    Add new client project

---
+++ client/next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  productionBrowserSourceMaps: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: process.env.PROTOCOL,
+        hostname: process.env.STRAPI_HOST,
+        pathname: "/uploads/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
