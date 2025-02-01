f361ecf293cb2c272da4ad795573bd4551db8eb4
skin93 <arro.ciesielski@gmail.com>
2025-02-01 23:48:27 +0100

    Add allFestivalsQuery

---
+++ client/lib/queries/festivals/allFestivalsQuery.js
@@ -0,0 +1,29 @@
+import { gql } from "graphql-request";
+export const ALL_FESTIVALS_QUERY = gql`
+  query {
+    festivals {
+      id
+      name
+      description
+      slug
+      location {
+        city
+        place
+        latitude
+        longitude
+      }
+      next_event {
+        name
+        date
+        from_date
+        to_date
+      }
+      image {
+        url
+        width
+        height
+        alternativeText
+      }
+    }
+  }
+`;
