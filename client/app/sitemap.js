4f4dd6541f7226c96051ea7770f8cd986c11bec5
skin93 <arro.ciesielski@gmail.com>
2025-02-09 18:45:13 +0100

    Generate sitemap

---
+++ client/app/sitemap.js
@@ -0,0 +1,33 @@
+import { fetcher } from "@/lib/fetcher";
+import { ARTICLES_SITEMAP_QUERY } from "@/lib/queries/articles/articlesSitemapQuery";
+
+export default async function sitemap() {
+  const { articles } = await fetcher(ARTICLES_SITEMAP_QUERY);
+  const articlesEntries = articles.map(({ slug, updatedAt }) => ({
+    url: `${process.env.NEXT_PUBLIC_APP_DOMAIN}/articles/${slug}`,
+    lastModified: new Date(updatedAt),
+    changeFrequency: "daily",
+    priority: 0.7,
+  }));
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_APP_DOMAIN}`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.7,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_APP_DOMAIN}/festival-map`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_APP_DOMAIN}/contact`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.3,
+    },
+    ...articlesEntries,
+  ];
+}
