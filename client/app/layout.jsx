508d9e30cb19ad68343503b7d2588e6febc7c8c3
skin93 <arro.ciesielski@gmail.com>
2025-02-09 23:08:01 +0100

    Set default theme to dark

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -51,7 +51,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
