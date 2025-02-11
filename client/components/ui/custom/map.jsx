839b35c361544dd10f8b94d460f78a2da7642174
skin93 <arro.ciesielski@gmail.com>
2025-02-11 19:36:56 +0100

    Change leaflet provider to stadia, display map again

--- client/components/ui/custom/map.jsx
+++ client/components/ui/custom/map.jsx
@@ -31,13 +31,14 @@ export default function Map({ markers }) {
       <MapContainer
         preferCanvas={true}
         center={[51.974077, 19.451946]}
+        maxZoom={20}
         zoom={6}
         scrollWheelZoom={true}
         className="w-[100svw] h-[calc(100svh-56px)]"
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
+          url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
         />
         <MarkerClusterGroup
           chunkedLoading
--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      // Basic redirect
-      {
-        source: "/festival-map",
-        destination: "/",
-        permanent: true,
-      },
-    ];
-  },
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
   images: {
