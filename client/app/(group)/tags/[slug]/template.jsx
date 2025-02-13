16373aa97e4e17f621d83e386df43fef5dbe6e70
skin93 <arro.ciesielski@gmail.com>
2025-02-13 20:39:42 +0100

    Modify transition

--- client/app/(group)/tags/[slug]/template.jsx
+++ client/app/(group)/tags/[slug]/template.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 export default function Template({ children }) {
   return (
     <motion.div
-      initial={{ y: 0, opacity: 0 }}
+      initial={{ y: 20, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ ease: "easeInOut", duration: 0.7 }}
     >
