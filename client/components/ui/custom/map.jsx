b843b001c768771f1d2d0cea552f1a2bcaf036e5
skin93 <arro.ciesielski@gmail.com>
2025-05-29 22:54:36 +0200

    Display filters in Dialog and fix Drawer visibility

--- client/components/ui/custom/map.jsx
+++ client/components/ui/custom/map.jsx
@@ -5,7 +5,7 @@ import { useRef, useState } from "react";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, Marker, Popup } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-markercluster";
-import { Dialog, DialogContent, DialogTrigger } from "../dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTrigger } from "../dialog";
 import { Button } from "../button";
 import Image from "next/image";
 import Link from "next/link";
@@ -31,6 +31,7 @@ import { MapLibreTileLayer } from "./map-libre-tile-layer";
 export default function Map({ markers, genres }) {
   const center = [51.974077, 19.451946];
   const zoom = 6;
+  const [dialog, setDialog] = useState(false);
   const [cityValue, setCityValue] = useState("");
   const [festValue, setFestValue] = useState("");
   const [genreValue, setGenreValue] = useState("");
@@ -193,144 +194,171 @@ export default function Map({ markers, genres }) {
             </Dialog>
           ))}
         </MarkerClusterGroup>
-        <div className="absolute top-3 right-3 z-500 flex xl:flex-row flex-col gap-4">
-          <Popover open={cityPopOpen} onOpenChange={setCityPopOpen}>
-            <PopoverTrigger asChild>
-              <Button
-                disabled={festValue !== "" || genreValue !== ""}
-                variant="outline"
-                role="combobox"
-                aria-expanded={cityPopOpen}
-                className="w-[200px] justify-between"
-              >
-                {cityValue
-                  ? [...cities].find((city) => city === cityValue)
-                  : "Wybierz miasto..."}
-                <ChevronsUpDown className="opacity-50" />
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-[200px] p-0">
-              <Command>
-                <CommandInput placeholder="Wybierz miasto..." className="h-9" />
-                <CommandList>
-                  <CommandEmpty>Brak miasta</CommandEmpty>
-                  <CommandGroup>
-                    {[...cities].sort().map((city) => (
-                      <CommandItem
-                        key={city}
-                        value={city}
-                        onSelect={(currentValue) =>
-                          handleCityChange(currentValue)
-                        }
-                      >
-                        {city}
-                        <Check
-                          className={cn(
-                            "ml-auto",
-                            cityValue === city ? "opacity-100" : "opacity-0"
-                          )}
-                        />
-                      </CommandItem>
-                    ))}
-                  </CommandGroup>
-                </CommandList>
-              </Command>
-            </PopoverContent>
-          </Popover>
-          <Popover open={festPopOpen} onOpenChange={setFestPopOpen}>
-            <PopoverTrigger asChild>
-              <Button
-                disabled={cityValue !== "" || genreValue !== ""}
-                variant="outline"
-                role="combobox"
-                aria-expanded={festPopOpen}
-                className="w-[200px] justify-between"
-              >
-                {festValue
-                  ? [...names].find((name) => name === festValue)
-                  : "Wybierz festiwal..."}
-                <ChevronsUpDown className="opacity-50" />
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-[200px] p-0">
-              <Command>
-                <CommandInput
-                  placeholder="Wybierz festiwal..."
-                  className="h-9"
-                />
-                <CommandList>
-                  <CommandEmpty>Brak festiwalu</CommandEmpty>
-                  <CommandGroup>
-                    {[...names].sort().map((name) => (
-                      <CommandItem
-                        key={name}
-                        value={name}
-                        onSelect={(currentValue) =>
-                          handleFestChange(currentValue)
-                        }
-                      >
-                        {name}
-                        <Check
-                          className={cn(
-                            "ml-auto",
-                            festValue === name ? "opacity-100" : "opacity-0"
-                          )}
-                        />
-                      </CommandItem>
-                    ))}
-                  </CommandGroup>
-                </CommandList>
-              </Command>
-            </PopoverContent>
-          </Popover>
-          <Popover open={genrePopOpen} onOpenChange={setGenrePopOpen}>
-            <PopoverTrigger asChild>
-              <Button
-                disabled={festValue !== "" || cityValue !== ""}
-                variant="outline"
-                role="combobox"
-                aria-expanded={genrePopOpen}
-                className="w-[200px] justify-between"
-              >
-                {genreValue
-                  ? [...genres].find((genre) => genre === genreValue)
-                  : "Wybierz gatunek..."}
-                <ChevronsUpDown className="opacity-50" />
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-[200px] p-0">
-              <Command>
-                <CommandInput
-                  placeholder="Wybierz gatunek..."
-                  className="h-9"
-                />
-                <CommandList>
-                  <CommandEmpty>Brak gatunku</CommandEmpty>
-                  <CommandGroup>
-                    {[...genres].sort().map((genre) => (
-                      <CommandItem
-                        key={genre}
-                        value={genre}
-                        onSelect={(currentValue) =>
-                          handleGenreChange(currentValue)
-                        }
-                      >
-                        {genre}
-                        <Check
-                          className={cn(
-                            "ml-auto",
-                            genreValue === genre ? "opacity-100" : "opacity-0"
-                          )}
-                        />
-                      </CommandItem>
-                    ))}
-                  </CommandGroup>
-                </CommandList>
-              </Command>
-            </PopoverContent>
-          </Popover>
-          <Button onClick={handleReset}>Reset</Button>
-        </div>
+        <Dialog open={dialog} onOpenChange={setDialog}>
+          <DialogTrigger asChild>
+            <Button
+              className="cursor-pointer z-900 absolute left-[50%] translate-x-[-50%] top-10  w-[200px]"
+              variant={"default"}
+              onClick={() => setDialog(true)}
+            >
+              Filtruj
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="border-none flex flex-col items-center justify-start max-w-[60em] h-130 md:h-120">
+            <DialogHeader>
+              <DialogTitle className="m-0">Filtruj mapę</DialogTitle>
+            </DialogHeader>
+            <DialogDescription className="m-0">
+              Wybierz jeden z poniszych filtrów
+            </DialogDescription>
+            <div className="flex flex-col justify-around md:flex-row gap-4">
+              <Popover open={cityPopOpen} onOpenChange={setCityPopOpen}>
+                <PopoverTrigger asChild>
+                  <Button
+                    disabled={festValue !== "" || genreValue !== ""}
+                    variant="outline"
+                    role="combobox"
+                    aria-expanded={cityPopOpen}
+                    className="xl:w-[200px] justify-between"
+                  >
+                    {cityValue
+                      ? [...cities].find((city) => city === cityValue)
+                      : "Wybierz miasto..."}
+                    <ChevronsUpDown className="opacity-50" />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="xl:w-[200px] p-0 pointer-events-auto">
+                  <Command>
+                    <CommandInput
+                      placeholder="Wybierz miasto..."
+                      className="h-9"
+                    />
+                    <CommandList className="h-50">
+                      <CommandEmpty>Brak miasta</CommandEmpty>
+                      <CommandGroup>
+                        {[...cities].sort().map((city) => (
+                          <CommandItem
+                            key={city}
+                            value={city}
+                            onSelect={(currentValue) => {
+                              handleCityChange(currentValue);
+                              setDialog(false);
+                            }}
+                          >
+                            {city}
+                            <Check
+                              className={cn(
+                                "ml-auto",
+                                cityValue === city ? "opacity-100" : "opacity-0"
+                              )}
+                            />
+                          </CommandItem>
+                        ))}
+                      </CommandGroup>
+                    </CommandList>
+                  </Command>
+                </PopoverContent>
+              </Popover>
+              <Popover open={festPopOpen} onOpenChange={setFestPopOpen}>
+                <PopoverTrigger asChild>
+                  <Button
+                    disabled={cityValue !== "" || genreValue !== ""}
+                    variant="outline"
+                    role="combobox"
+                    aria-expanded={festPopOpen}
+                    className="xl:w-[200px] justify-between"
+                  >
+                    {festValue
+                      ? [...names].find((name) => name === festValue)
+                      : "Wybierz festiwal..."}
+                    <ChevronsUpDown className="opacity-50" />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="xl:w-[200px] p-0 pointer-events-auto">
+                  <Command>
+                    <CommandInput
+                      placeholder="Wybierz festiwal..."
+                      className="h-9"
+                    />
+                    <CommandList className="h-50">
+                      <CommandEmpty>Brak festiwalu</CommandEmpty>
+                      <CommandGroup>
+                        {[...names].sort().map((name) => (
+                          <CommandItem
+                            key={name}
+                            value={name}
+                            onSelect={(currentValue) => {
+                              handleFestChange(currentValue);
+                              setDialog(false);
+                            }}
+                          >
+                            {name}
+                            <Check
+                              className={cn(
+                                "ml-auto",
+                                festValue === name ? "opacity-100" : "opacity-0"
+                              )}
+                            />
+                          </CommandItem>
+                        ))}
+                      </CommandGroup>
+                    </CommandList>
+                  </Command>
+                </PopoverContent>
+              </Popover>
+              <Popover open={genrePopOpen} onOpenChange={setGenrePopOpen}>
+                <PopoverTrigger asChild>
+                  <Button
+                    disabled={festValue !== "" || cityValue !== ""}
+                    variant="outline"
+                    role="combobox"
+                    aria-expanded={genrePopOpen}
+                    className="xl:w-[200px] justify-between"
+                  >
+                    {genreValue
+                      ? [...genres].find((genre) => genre === genreValue)
+                      : "Wybierz gatunek..."}
+                    <ChevronsUpDown className="opacity-50" />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="xl:w-[200px] p-0 pointer-events-auto">
+                  <Command>
+                    <CommandInput
+                      placeholder="Wybierz gatunek..."
+                      className="h-9"
+                    />
+                    <CommandList className="h-50">
+                      <CommandEmpty>Brak gatunku</CommandEmpty>
+                      <CommandGroup>
+                        {[...genres].sort().map((genre) => (
+                          <CommandItem
+                            key={genre}
+                            value={genre}
+                            onSelect={(currentValue) => {
+                              handleGenreChange(currentValue);
+                              setDialog(false);
+                            }}
+                          >
+                            {genre}
+                            <Check
+                              className={cn(
+                                "ml-auto",
+                                genreValue === genre
+                                  ? "opacity-100"
+                                  : "opacity-0"
+                              )}
+                            />
+                          </CommandItem>
+                        ))}
+                      </CommandGroup>
+                    </CommandList>
+                  </Command>
+                </PopoverContent>
+              </Popover>
+              <Button onClick={handleReset}>Reset</Button>
+            </div>
+          </DialogContent>
+        </Dialog>
       </MapContainer>
 
       <div className="absolute bottom-8 right-0 sm:bottom-0 sm:left-0 z-500 text-neutral-700 bg-neutral-100 font-normal px-[5px] text-[12px] w-fit">
