b98c285d4d16d813a06d87a12e028cc64774c109
skin93 <arro.ciesielski@gmail.com>
2025-05-30 21:25:49 +0200

    Create the Web App Manifest

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -51,6 +51,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="FleszEvents" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
