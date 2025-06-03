2180404d54d409c727609bfa845236c0969ac50f
skin93 <arro.ciesielski@gmail.com>
2025-06-03 07:33:23 +0200

    Redirect fo festival-map

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -11,6 +11,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/festival-map",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
