c1ed12e7bb62d8d8968e68dd24d1e5cd2e1a4c19
skin93 <arro.ciesielski@gmail.com>
2025-06-15 23:07:15 +0200

    Remove Strict-Transport-Security

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -17,10 +17,6 @@ const nextConfig = {
         source: "/(.*)",
         headers: [
           {
-            key: "Strict-Transport-Security",
-            value: "max-age=31536000; includeSubDomains; preload",
-          },
-          {
             key: "X-Frame-Options",
             value: "DENY",
           },
